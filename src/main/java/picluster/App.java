135006b7d65847dd5e37a5786b39530bfded766d
Freddie Coleman <f.coleman@hotmail.co.uk>
2015-04-05 18:51:35 +0100

    update title of project

--- src/main/java/picluster/App.java
+++ src/main/java/picluster/App.java
@@ -9,7 +9,7 @@ public class App {
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                JFrame frame = new MainFrame("Hello World Swing!");
+                JFrame frame = new MainFrame("Shakespeare Pi Cluster!");
                 frame.setSize(500, 400);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.setVisible(true);
